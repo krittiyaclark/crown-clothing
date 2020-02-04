345783bce5f124c58ca91846c1b3f5a7275fe33c
krittiyaclark <krittiyaclark@gmail.com>
2020-02-04 16:52:29 -0500

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { 
+    clearItemFromCart, 
+    addItem, 
+    removeItem 
+} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckOutItem = ({ cartItem: { name, imageUrl, price, quantity }}) => {
+const CheckOutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
     return (
         <div className='checkout-item'>
             <div className='image-container'>
@@ -10,20 +18,29 @@ const CheckOutItem = ({ cartItem: { name, imageUrl, price, quantity }}) => {
             </div>
             <span className='name'>{name}</span>
             <span className='quantity'>
-                <div className='arrow' >
+                <div className='arrow' onClick={() => removeItem(cartItem)}>
                 &#10094;
                 </div>
                 <span className='value'>{quantity}</span>
-                <div className='arrow' >
+                <div className='arrow' onClick={() => addItem(cartItem)}>
                 &#10095;
                 </div>
             </span>
             <span className='price'>{price}</span>
-            <div className='remove-button' >
+            <div className='remove-button' onClick={() => clearItem(cartItem)} >
                 &#10005;
             </div>
         </div>
     )
 }
 
-export default CheckOutItem;
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item)) 
+}); 
+
+export default connect(
+    null, 
+    mapDispatchToProps
+)(CheckOutItem);
