d6fb95bfc3568aff0d5ac59e17d3c19d48c61645
krittiyaclark <krittiyaclark@gmail.com>
2020-02-07 06:47:52 -0500

    solved title the issue on the hats page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,11 +1,4 @@
 import { createSelector } from 'reselect';
-const COLLECTION_ID_MAP = {
-    hat: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
 
 const selectShop = state => state.shop;
 
@@ -17,5 +10,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam => 
 createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-)
+    collections => collections[collectionUrlParam]
+);
