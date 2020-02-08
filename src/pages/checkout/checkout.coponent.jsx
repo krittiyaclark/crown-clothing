99fafe993eb61500c0be943b9062fb52e6c8f276
krittiyaclark <krittiyaclark@gmail.com>
2020-02-08 12:19:14 -0500

    updating stripe integration and updating our favicon and title

--- src/pages/checkout/checkout.coponent.jsx
+++ src/pages/checkout/checkout.coponent.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import CheckOutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckOutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -10,7 +11,6 @@ import {
 } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
-import CheckOutItem from '../../components/checkout-item/checkout-item.component';
 
 const CheckOutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -35,9 +35,13 @@ const CheckOutPage = ({ cartItems, total }) => (
             <CheckOutItem key={cartItem.id} cartItem={cartItem} />
         ))}
 
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following test creatdit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 09/20 - CVV: 123
         </div>
+        <StripeCheckOutButton price={total} />
     </div>
 )
 
