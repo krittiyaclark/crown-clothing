56891f9910020dd9968b3b2daf3bc199fa7c705b
krittiyaclark <krittiyaclark@gmail.com>
2020-02-11 09:40:48 -0500

    styled-components and adding Shop Data to Redux

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
+import { updateCollections } from '../../redux/shop/shop.actions';
+
 class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+        const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
         
-        collectionRef.onSnapshot(async snapshot => {
-            convertCollectionsSnapshotToMap(snapshot);
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+         const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+         updateCollections(collectionMap);
         });
     }
 
@@ -26,7 +31,11 @@ class ShopPage extends React.Component {
             </div>
         )
     }
-
 };
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap =>
+      dispatch(updateCollections(collectionsMap))
+  });
+
+export default connect(null, mapDispatchToProps)(ShopPage);
