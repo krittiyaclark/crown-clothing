a8fbb27efe1d6116b55182b8715955eab0b89650
krittiyaclark <krittiyaclark@gmail.com>
2020-02-25 12:48:06 -0500

    Redux-Saga

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -2,43 +2,39 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.continer';
-import CollectionPageContainer from '../collection/collection.component';
+import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
-    componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
-        fetchCollectionsStartAsync();
-    }
+	componentDidMount() {
+		const { fetchCollectionsStart } = this.props;
 
-    render () {
-        const { match } = this.props;
-        return (
-            <div className='shop-page'>
-                <Route 
-                    exact path={`${match.path}`} 
-                    component={CollectionsOverviewContainer}  
-                />
-                    )}    
-                />
-                <Route 
-                    path={`${match.path}/:collectionId`} 
-                    component={CollectionPageContainer}
-                />
-                    )}
-                />
-            </div>
-        )
-    }
-};
+		fetchCollectionsStart();
+	}
+
+	render() {
+		const { match } = this.props;
+
+		return (
+			<div className='shop-page'>
+				<Route
+					exact
+					path={`${match.path}`}
+					component={CollectionsOverviewContainer}
+				/>
+				<Route
+					path={`${match.path}/:collectionId`}
+					component={CollectionPageContainer}
+				/>
+			</div>
+		);
+	}
+}
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+	fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
