0b6dc8c01d930a3010a44098a1b48c98dda3c1e3
krittiyaclark <krittiyaclark@gmail.com>
2020-02-26 07:14:38 -0500

    Google Sign In Into Sagas

--- src/App.js
+++ src/App.js
@@ -17,62 +17,59 @@ import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-  unsubscribeFromAuth = null;
+	unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
+	componentDidMount() {
+		const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+		// this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+		//   if (userAuth) {
+		//     const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          })
-        });
-      }
+		//     userRef.onSnapshot(snapShot => {
+		//       setCurrentUser({
+		//           id: snapShot.id,
+		//           ...snapShot.data()
+		//       })
+		//     });
+		//   }
 
-      setCurrentUser(userAuth);
-    });
-  }
+		//   setCurrentUser(userAuth);
+		// });
+	}
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckOutPage} />
-          
-          <Route 
-            exact 
-            path='/signIn' 
-            render={() => 
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            } 
-          />
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route exact path='/checkout' component={CheckOutPage} />
+
+					<Route
+						exact
+						path='/signIn'
+						render={() =>
+							this.props.currentUser ? (
+								<Redirect to='/' />
+							) : (
+								<SignInAndSignUpPage />
+							)
+						}
+					/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+	currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,80 +3,79 @@ import 'firebase/firebase-firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyB-u3xKMZCZLSz3kuS0eUL5M-3LQ6b2NMA",
-    authDomain: "crown-db-e579d.firebaseapp.com",
-    databaseURL: "https://crown-db-e579d.firebaseio.com",
-    projectId: "crown-db-e579d",
-    storageBucket: "crown-db-e579d.appspot.com",
-    messagingSenderId: "909189678556",
-    appId: "1:909189678556:web:ebd6f28c13fafd5c92fb19",
-    measurementId: "G-RTGMGPD22C"
-}
+	apiKey: 'AIzaSyB-u3xKMZCZLSz3kuS0eUL5M-3LQ6b2NMA',
+	authDomain: 'crown-db-e579d.firebaseapp.com',
+	databaseURL: 'https://crown-db-e579d.firebaseio.com',
+	projectId: 'crown-db-e579d',
+	storageBucket: 'crown-db-e579d.appspot.com',
+	messagingSenderId: '909189678556',
+	appId: '1:909189678556:web:ebd6f28c13fafd5c92fb19',
+	measurementId: 'G-RTGMGPD22C'
+};
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+	if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = await userRef.get();
+	const snapShot = await userRef.get();
 
-    if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
 
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            })
-        }   catch (error) {
-            console.log('error creating user', error.message)
-        }
-    }
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
 
-    return userRef;
-}
+	return userRef;
+};
 
 export const addCollectionAndDocuments = async (collectionKey, objectToAdd) => {
-    const collectionRef = firestore.collection(collectionKey);
+	const collectionRef = firestore.collection(collectionKey);
 
-    const batch = firestore.batch();
-    objectToAdd.forEach(obj => {
-        const newDocRef = collectionRef.doc();
-        batch.set(newDocRef, obj);
-    });
+	const batch = firestore.batch();
+	objectToAdd.forEach(obj => {
+		const newDocRef = collectionRef.doc();
+		batch.set(newDocRef, obj);
+	});
 
-    return await batch.commit()
+	return await batch.commit();
 };
 
 export const convertCollectionsSnapshotToMap = collections => {
-    const transformedCollection = collections.docs.map(doc => {
-        const { title, items } = doc.data();
+	const transformedCollection = collections.docs.map(doc => {
+		const { title, items } = doc.data();
 
-        return {
-            routeName: encodeURI(title.toLowerCase()),
-            id: doc.id,
-            title,
-            items
-            };
-    });
+		return {
+			routeName: encodeURI(title.toLowerCase()),
+			id: doc.id,
+			title,
+			items
+		};
+	});
 
-    return transformedCollection.reduce((accumulator, collection) => {
-        accumulator[collection.title.toLowerCase()] = collection;
-        return accumulator;
-    }, {});
-
+	return transformedCollection.reduce((accumulator, collection) => {
+		accumulator[collection.title.toLowerCase()] = collection;
+		return accumulator;
+	}, {});
 };
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
